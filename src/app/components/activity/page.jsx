3b1f31111c605c9fadcadbaed75372b9a15b1774
Marc_dev <marcramadison006@gmail.com>
2025-01-23 16:19:23 +0100

    finish activity menu

--- src/app/components/activity/page.jsx
+++ src/app/components/activity/page.jsx
@@ -1,38 +1,6 @@
 import style from "./activity.module.css";
 import Image from "next/image";
-
-const listActivity = [
-  {
-    logoUrl: "/assets/logo_online.png",
-    illustrationUrl: "/assets/mzo_online.png",
-    descritption:
-      "Découvrez Mi-Zesta Online, notre boutique de mode en ligne, avec des collections pour toute la famille et des ventes en direct sur les réseaux sociaux.",
-  },
-  {
-    logoUrl: "/assets/logo_contact_center.png",
-    illustrationUrl: "/assets/mzo_contact.png",
-    descritption:
-      "Optimisez vos performances avec MZO Contact Center, expert en externalisation et offshoring",
-  },
-  {
-    logoUrl: "/assets/logo_provider.png",
-    illustrationUrl: "/assets/mzo_provider.png",
-    descritption:
-      "Profitez d’une connectivité satellitaire fiable, même dans les zones reculées, grâce à MZO Starlink Provider.",
-  },
-  {
-    logoUrl: "/assets/logo_event.png",
-    illustrationUrl: "/assets/mzo_event.png",
-    descritption:
-      "Transformez vos festivités en événements inoubliables avec MZO Event et ses solutions clé en main.",
-  },
-  {
-    logoUrl: "/assets/logo_business.png",
-    illustrationUrl: "/assets/mzo_business.png",
-    descritption:
-      "Simplifiez vos échanges commerciaux entre la Chine et Madagascar avec MZO Business et ses services d'importation et transit sur mesure.",
-  },
-];
+import { listActivity } from "@/app/utils";
 
 function Activity() {
   return (
--- src/app/components/menuBar/page.js
+++ src/app/components/menuBar/page.js
@@ -1,11 +1,14 @@
 "use client";
 
+import { listActivity } from "@/app/utils";
 import style from "./menuBar.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
 function MenuBar() {
   const [toggleMobile, setToggleMobile] = useState(false);
+  const [toggleActivity, setToggleActivity] = useState(false);
+
   const toggleMenu = () => {
     const idMenu = document.querySelector("#menuBlockId");
     const burgerMenu = document.querySelector("#burger_menu_id");
@@ -17,11 +20,34 @@ function MenuBar() {
         spans[1].style.opacity = "";
         spans[2].style.transform = "";
         idMenu.style.height = "0px";
+        idMenu.style.opacity = "0";
+
       } else {
         spans[0].style.transform = "translateY(13px) rotate(45deg)";
         spans[1].style.opacity = "0";
         spans[2].style.transform = "translateY(-13px) rotate(-45deg)";
         idMenu.style.height = "100px";
+        idMenu.style.opacity = "1";
+
+      }
+
+      return !prev;
+    });
+  };
+
+  const handleActivityClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const sousMenu = document.querySelector("#sousMenu");
+
+    setToggleActivity((prev) => {
+      if (prev) {
+        sousMenu.style.height = "0px";
+        sousMenu.style.opacity = "0";
+      } else {
+        sousMenu.style.height = "150px";
+        sousMenu.style.opacity = "1";
       }
 
       return !prev;
@@ -29,28 +55,47 @@ function MenuBar() {
   };
 
   return (
-    <>
-      <div className={style.container}>
-        <Image
-          className={style.logo}
-          src="/assets/mzo_logo.png"
-          alt="mzo logo"
-          width={120}
-          height={80}
-        />
-        <div className={style.menuItems}>
-          <h5 className={style.menuItem}>Acceuil</h5>
-          <h5 className={style.activity}>Activité</h5>
-          <h5 className={style.menuItem}>Contact</h5>
-          <div className={style.doteContainer}>
-            <div className={style.dote}></div>
-            <div className={style.dote}></div>
-            <div className={style.dote}></div>
+    <div>
+      <div style={{display:'flex',flexDirection:'column'}}>
+        <div className={style.container}>
+          <Image
+            className={style.logo}
+            src="/assets/mzo_logo.png"
+            alt="mzo logo"
+            width={120}
+            height={80}
+          />
+          <div className={style.menuItems}>
+            <h5 className={style.menuItem}>Acceuil</h5>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+              id="activity"
+              onClick={handleActivityClick}
+            >
+              <h5 className={style.activity}>Activité</h5>
+              {toggleActivity? <div className={style.arrow_up}></div> :  <div className={style.arrow_down}></div>}
+            </div>
+
+            <h5 className={style.menuItem}>Contact</h5>
+            <div className={style.doteContainer}>
+              <div className={style.dote}></div>
+              <div className={style.dote}></div>
+              <div className={style.dote}></div>
+            </div>
           </div>
         </div>
+        <div className={style.sous_menu} id="sousMenu">
+          {listActivity.map((activity,index)=>{
+            return <Image key={index} src={activity.logoUrl} width={120} height={70} alt="logo mzo activity"/>
+          })}
+        </div>
       </div>
-      <div className={style.sous_menu}></div>
-      <div className={style.mobileBlock}>
+
+      <div className={style.mobileBlock} id="mobileBlockId">
         <div className={style.mobileContainer}>
           <Image
             src="/assets/mzo_logo.png"
@@ -58,7 +103,11 @@ function MenuBar() {
             width={100}
             height={60}
           />
-          <div className={style.burger_menu} id="burger_menu_id" onClick={toggleMenu}>
+          <div
+            className={style.burger_menu}
+            id="burger_menu_id"
+            onClick={toggleMenu}
+          >
             <span></span>
             <span></span>
             <span></span>
@@ -66,7 +115,7 @@ function MenuBar() {
         </div>
         <div className={style.menuBlock} id="menuBlockId"></div>
       </div>
-    </>
+    </div>
   );
 }
 
