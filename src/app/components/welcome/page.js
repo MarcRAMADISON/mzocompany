905634ba645d3084429d5913564667cd396a9292
Marc_dev <marcramadison006@gmail.com>
2025-01-24 15:08:04 +0100

    animate image logo

--- src/app/components/welcome/page.js
+++ src/app/components/welcome/page.js
@@ -11,6 +11,13 @@ function Welcome() {
         height={600}
         className={style.image}
       />
+      <Image
+        src="/assets/background_welcome_page.png"
+        alt="welcome mzo"
+        width={1080}
+        height={920}
+        className={style.filigrane}
+      />
       <h5 className={style.title}>VOTRE PARTENAIRE</h5>
       <h6 className={style.subTitle}>MULTISECTORIEL DE CONFIANCE</h6>
       <div className="pageDoteContainer">
