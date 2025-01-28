0a6f3df5e17db6693acacfb62d615dc11fb50ff8
Marc_dev <marcramadison006@gmail.com>
2025-01-28 15:39:53 +0100

    add image carousel

--- src/app/components/about/page.jsx
+++ src/app/components/about/page.jsx
@@ -3,16 +3,28 @@ import Image from "next/image";
 import CustomCarousel from "../carousel/page";
 import ScrollAppear from "../scrollAppear/page";
 
-const data=[
-    "/assets/mzo_contact.png",
-    "/assets/mzo_event.png",
-    "/assets/mzo_business.png",
-    "/assets/mzo_provider.png",
-    "/assets/mzo_contact.png",
-    "/assets/mzo_event.png",
-    "/assets/mzo_business.png",
-    "/assets/mzo_provider.png",
-]
+const data = [
+  "/assets/carrousel1.jpeg",
+  "/assets/carrousel2.jpeg",
+  "/assets/carrousel3.jpeg",
+  "/assets/carrousel4.jpeg",
+  "/assets/carrousel5.jpeg",
+  "/assets/carrousel6.jpeg",
+  "/assets/carrousel7.jpeg",
+  "/assets/carrousel8.jpeg",
+  "/assets/carrousel9.jpeg",
+  "/assets/carrousel10.jpeg",
+  "/assets/carrousel11.jpeg",
+  "/assets/carrousel12.jpeg",
+  "/assets/carrousel13.jpeg",
+  "/assets/carrousel14.jpeg",
+  "/assets/carrousel15.jpeg",
+  "/assets/carrousel16.jpeg",
+  "/assets/carrousel17.jpeg",
+  "/assets/carrousel18.jpeg",
+  "/assets/carrousel19.jpeg",
+  "/assets/carrousel20.jpeg",
+];
 
 function About() {
   return (
@@ -24,10 +36,14 @@ function About() {
         height={600}
         className={style.image}
       />
-      <h5 className={style.title}>" Au cœur de vos projets, à la source de vos succès. "</h5>
-        <CustomCarousel rows={data}/>
+      <h5 className={style.title}>
+        " Au cœur de vos projets, à la source de vos succès. "
+      </h5>
+      <CustomCarousel rows={data} />
       <h5 className={style.sousTitle}>Avec Mzo Company,</h5>
-      <h6 className={style.text}>Chaque service est conçu pour répondre à vos besoins spécifiques.</h6>
+      <h6 className={style.text}>
+        Chaque service est conçu pour répondre à vos besoins spécifiques.
+      </h6>
     </div>
   );
 }
--- src/app/components/carousel/page.jsx
+++ src/app/components/carousel/page.jsx
@@ -4,19 +4,18 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import style from "./carousel.module.css";
 import Image from "next/image";
-import ScrollAppear from "../scrollAppear/page";
 
 const CustomCarousel = ({ rows = [] }) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 4,
-      slidesToSlide: 3, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -34,7 +33,7 @@ const CustomCarousel = ({ rows = [] }) => {
       ssr={true} // means to render carousel on server-side.
       infinite={true}
       autoPlay={true}
-      autoPlaySpeed={1000}
+      autoPlaySpeed={3000}
       keyBoardControl={true}
       customTransition="all .5"
       transitionDuration={500}
@@ -46,13 +45,12 @@ const CustomCarousel = ({ rows = [] }) => {
     >
       {(rows || []).map((row, index) => {
         return (
-            <div key={index}>
+            <div key={index} style={{ position:'relative',width: "90%", height: "400px",placeSelf:'center' }}>
               <Image
                 src={row}
                 alt="image about mzo"
-                width={200}
-                height={300}
-                style={{ width: "90%", height: "auto" }}
+                layout="fill"
+                objectFit="cover"
               />
             </div>
         );
