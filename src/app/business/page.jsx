fc9ff064434ef65a6d85b37e464ed48a66434a0f
Marc_dev <marcramadison006@gmail.com>
2025-02-17 15:56:19 +0100

    begin mzo event,mzo business page

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -1,9 +1,57 @@
 import MenuBar from "../components/menuBar/page";
+import styles from './business.module.css'
+import Image from "next/image";
 
 
 function BusenessPage() {
-    return ( <div>
+    return ( <div style={{display:"flex",flexDirection:'column',alignItems:"center",paddingBottom: "100px",}}>
         <MenuBar/>
+        <div className={styles.firstContainer}>
+        <Image
+          src="/assets/background_welcome_page.png"
+          alt="welcome mzo"
+          width={1080}
+          height={920}
+          className={styles.filigrane}
+        />
+        <div className={styles.logoMzo}>
+          <Image
+            src="/assets/large_mzo_business.png"
+            alt="logo mzo starlink provider"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="pageDoteContainer">
+          <div className="pageDote"></div>
+          <div className="pageDote"></div>
+          <div className="pageDote"></div>
+        </div>
+      
+      </div>
+      <span className={styles.titleDescription}>
+        Avec plus de 7 ans d'expérience, MZO Event est votre partenaire de confiance pour organiser des événements inoubliables : mariages, anniversaires, team building, communions, et bien plus. Nous proposons des solutions clé en main avec la location de sonorisation, de lumières, de musique orchestrale, ainsi que la tête de cortège pour des entrées spectaculaires. Pour sublimer votre événement, nous offrons également des services tels que les effets spéciaux, l’installation d’estrades et de scènes, l’animation par un DJ renommé, ainsi que la présence de photographes, de vidéastes et de prises de vue par drone. Faites confiance à MZO Event pour créer des souvenirs uniques et transformer chaque célébration en un moment mémorable.
+        </span>
+        <div className={styles.valeursContainer}>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Sécurité</span>
+          <span className={styles.valeur}>
+          La protection optimale de vos marchandises durant toute la chaîne logistique.
+          </span>
+        </div>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Simplicité</span>
+          <span className={styles.valeur}>
+          Des solutions simples et pratiques pour un processus d’importation sans tracas.
+          </span>
+        </div>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Responsabilité</span>
+          <span className={styles.valeur}>
+          Une gestion des affaires qui respecte les normes et les régulations internationales.
+          </span>
+        </div>
+      </div>
     </div> );
 }
 
--- src/app/event/page.jsx
+++ src/app/event/page.jsx
@@ -1,10 +1,78 @@
 import MenuBar from "../components/menuBar/page";
-
+import styles from "./event.module.css";
+import Image from "next/image";
 
 function EventPage() {
-    return ( <div>
-        <MenuBar/>
-    </div> );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingBottom: "100px",
+      }}
+    >
+      <MenuBar />
+      <div className={styles.firstContainer}>
+        <Image
+          src="/assets/background_welcome_page.png"
+          alt="welcome mzo"
+          width={1080}
+          height={920}
+          className={styles.filigrane}
+        />
+        <div className={styles.logoMzo}>
+          <Image
+            src="/assets/large_mzo_event.png"
+            alt="logo mzo starlink provider"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="pageDoteContainer">
+          <div className="pageDote"></div>
+          <div className="pageDote"></div>
+          <div className="pageDote"></div>
+        </div>
+      </div>
+      <span className={styles.titleDescription}>
+        Avec plus de 7 ans d'expérience, MZO Event est votre partenaire de
+        confiance pour organiser des événements inoubliables : mariages,
+        anniversaires, team building, communions, et bien plus. Nous proposons
+        des solutions clé en main avec la location de sonorisation, de lumières,
+        de musique orchestrale, ainsi que la tête de cortège pour des entrées
+        spectaculaires. Pour sublimer votre événement, nous offrons également
+        des services tels que les effets spéciaux, l’installation d’estrades et
+        de scènes, l’animation par un DJ renommé, ainsi que la présence de
+        photographes, de vidéastes et de prises de vue par drone. Faites
+        confiance à MZO Event pour créer des souvenirs uniques et transformer
+        chaque célébration en un moment mémorable.
+      </span>
+      <div className={styles.valeursContainer}>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Fiabilité</span>
+          <span className={styles.valeur}>
+            Un service fiable et ponctuel, garantissant une organisation sans
+            stress.
+          </span>
+        </div>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Créativité</span>
+          <span className={styles.valeur}>
+            Des idées originales pour personnaliser chaque événement et le
+            rendre unique.
+          </span>
+        </div>
+        <div className={styles.valeurItem}>
+          <span className={styles.valeurTitle}>Passion</span>
+          <span className={styles.valeur}>
+            Un engagement total pour faire de chaque événement un moment
+            exceptionnel.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EventPage;
